84224a2bb642918f6084f6ecdd3310a04a0b3f89
zlatonn <tonkar2541@hotmail.com>
2025-01-14 22:46:03 +0700

    chore: routing  management
    - install react-router-dom
    - define path router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NavBar from "./components/navbar/Navbar";
+import ItemList from "./pages/ItemList";
+import ItemCreate from "./pages/ItemCreate";
+import ItemDetail from "./pages/ItemDetail";
 
-function App() {
+const App = () => {
   return (
-    <div className="min-h-screen p-0 m-0 flex flex-col items-center">
-      <NavBar />
-    </div>
+    <Router>
+      <div className="min-h-screen p-0 m-0 flex flex-col items-center">
+        <NavBar />
+        <div>
+          <Routes>
+            <Route path="/" element={<ItemList />} />
+            <Route path="/create" element={<ItemCreate />} />
+            <Route path="/detail" element={<ItemDetail />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
