f1a3241e785150a0ebb39e273dd66066745e6528
zlatonn <tonkar2541@hotmail.com>
2025-01-14 22:52:11 +0700

    chore: link page
    - apply Link at navBar component

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import searchIcon from "../../assets/search_icon.png";
 
 const NavBar = () => {
@@ -6,18 +7,20 @@ const NavBar = () => {
       {/* Header */}
       <div className="flex flex-col p-5 gap-3 sm:flex-row sm:items-center sm:justify-between">
         {/* icon & application name*/}
-        <div className="flex items-center gap-3">
-          <div className="w-10 sm:w-12 lg:w-14 ">
-            <img
-              src="https://i.pinimg.com/736x/c0/98/11/c09811f91b1b1dae29e16cc782aaf48a.jpg "
-              className="object-cover rounded-lg border-[1px] border-gray-300"
-            />
+        <Link to="/">
+          <div className="flex items-center gap-3">
+            <div className="w-10 sm:w-12 lg:w-14 ">
+              <img
+                src="https://i.pinimg.com/736x/c0/98/11/c09811f91b1b1dae29e16cc782aaf48a.jpg "
+                className="object-cover rounded-lg border-[1px] border-gray-300"
+              />
+            </div>
+            <div className="flex flex-col text-gray-800 font-bold text-lg sm:text-xl lg:text-2xl">
+              <p>warranty</p>
+              <p>checker</p>
+            </div>
           </div>
-          <div className="flex flex-col text-gray-800 font-bold text-lg sm:text-xl lg:text-2xl">
-            <p>warranty</p>
-            <p>checker</p>
-          </div>
-        </div>
+        </Link>
         {/* search box & create button */}
         <div className="flex items-center justify-between gap-3">
           <div className="flex-1 flex-grow-2 flex items-center justify-between bg-white border-[1px] border-gray-200 rounded-xl overflow-hidden">
@@ -30,9 +33,11 @@ const NavBar = () => {
               <img src={searchIcon} className="object-cover" />
             </button>
           </div>
-          <button className="h-10 px-4  inline-flex justify-center items-center bg-blue-500 duration-300 hover:bg-blue-300 text-white rounded-xl">
-            +
-          </button>
+          <Link to="/create">
+            <button className="h-10 px-4  inline-flex justify-center items-center bg-blue-500 duration-300 hover:bg-blue-300 text-white rounded-xl">
+              +
+            </button>
+          </Link>
         </div>
       </div>
 
