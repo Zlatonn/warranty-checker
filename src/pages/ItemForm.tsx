0d5fd59e557eb175bc1afe34baf0832a291df911
zlatonn <tonkar2541@hotmail.com>
2025-01-24 18:43:53 +0700

    style: apply style to ItemForm component

--- src/pages/ItemForm.tsx
+++ src/pages/ItemForm.tsx
@@ -1,6 +1,7 @@
 import { useState, ChangeEvent, useEffect } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import { useGetItem, useCreateItem, useUpdateItem, useDeleteItem } from "../hooks/useApi";
+import { AxiosError } from "axios";
 
 // Define type form
 interface Iform {
@@ -129,17 +130,49 @@ const ItemForm = () => {
   // Return JSX with loading condition
   if (isLoading) {
     return (
-      <div className="mt-10">
-        <p className="text-2xl text-gray-800">Loading...</p>
+      <div className="py-5 flex flex-col gap-5 lg:px-40">
+        <div className="skeleton h-8 w-1/4"></div>
+        <div className="skeleton h-4 w-full"></div>
+        <div className="skeleton h-4 w-full"></div>
+        <hr />
+        <div className="flex flex-col gap-8">
+          <div className="flex flex-col gap-3">
+            <div className="skeleton h-4 w-1/6"></div>
+            <div className="skeleton h-6 w-1/4"></div>
+          </div>
+          <div className="flex flex-col gap-3">
+            <div className="skeleton h-4 w-1/6"></div>
+            <div className="skeleton h-6 w-1/4"></div>
+          </div>
+          <div className="flex flex-col gap-3">
+            <div className="skeleton h-4 w-1/6"></div>
+            <div className="skeleton h-6 w-1/4"></div>
+          </div>
+          <div className="flex flex-col gap-3">
+            <div className="skeleton h-4 w-1/6"></div>
+            <div className="skeleton h-20 w-full"></div>
+          </div>
+        </div>
+        <div className="mt-10 flex justify-end gap-3">
+          <div className="skeleton h-8 w-20"></div>
+          <div className="skeleton h-8 w-20"></div>
+          <div className="skeleton h-8 w-20"></div>
+        </div>
       </div>
     );
   }
 
   // Return JSX with error condition
   if (isError) {
+    const axiosError = error as AxiosError; //Change error to AxiosError Type
     return (
-      <div className="mt-10">
-        <p className="text-2xl text-red-500">{error.message}</p>
+      <div className="mt-10 flex flex-col items-center gap-5">
+        {axiosError.request ? (
+          <p className="text-5xl text-gray-500">500</p>
+        ) : (
+          <p className="text-5xl text-gray-500">{axiosError.response?.status}</p>
+        )}
+        <p className="text-xl text-gray-400">{axiosError.message}</p>
       </div>
     );
   }
@@ -154,6 +187,7 @@ const ItemForm = () => {
             ? "This feature allows users to add a new item to the system. You can input the item's details such as name, serial number, warranty end date, and any additional notes. Once submitted, the item will be saved and displayed in the item list for tracking."
             : "This feature enables users to update the details of an existing item. You can modify the item's name, serial number, warranty information, or notes. This ensures that item details remain accurate and up to date in the system."}
         </p>
+
         <hr />
       </div>
       <div className="flex flex-col gap-8 text-gray-500">
@@ -165,7 +199,7 @@ const ItemForm = () => {
             name="itemName"
             value={formData?.itemName}
             onChange={handleInputChange}
-            className="w-full px-2 py-1 rounded-lg border-[1px] focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500"
+            className="input input-bordered w-full max-w-xs h-10"
           />
           {errors.itemName && <p className="w-fit mt-1 text-red-500 text-sm">{errors.itemName}</p>}
         </div>
@@ -177,7 +211,7 @@ const ItemForm = () => {
             name="serialNumber"
             value={formData?.serialNumber}
             onChange={handleInputChange}
-            className="w-full px-2 py-1 rounded-lg border-[1px] focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500"
+            className="input input-bordered w-full max-w-xs h-10"
           />
           {errors.serialNumber && <p className="w-fit mt-1 text-red-500 text-sm">{errors.serialNumber}</p>}
         </div>
@@ -188,7 +222,7 @@ const ItemForm = () => {
             name="endDate"
             value={formData?.endDate}
             onChange={handleInputChange}
-            className="w-full px-2 py-1 rounded-lg border-[1px] focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500"
+            className="input input-bordered w-full max-w-xs h-10"
           />
           {errors.endDate && <p className="w-fit mt-1 text-red-500 text-sm">{errors.endDate}</p>}
         </div>
@@ -199,27 +233,27 @@ const ItemForm = () => {
             name="notes"
             value={formData?.notes}
             onChange={handleInputChange}
-            className="w-full px-2 py-1 rounded-lg border-[1px] focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500"
+            className="textarea textarea-bordered"
           />
           {errors.notes && <p className="w-fit mt-1 text-red-500 text-sm">{errors.notes}</p>}
         </div>
       </div>
-      <div className="flex justify-end gap-3">
+      <div className="mt-10 flex justify-end gap-3">
         <Link to="/">
-          <button className="p-2 w-24 bg-[#d9d9d9] rounded-lg">Back</button>
+          <button className="btn p-2 w-24 bg-[#d9d9d9] rounded-lg">Back</button>
         </Link>
         {!itemId ? (
           <>
-            <button onClick={handleSubmit} className="p-2 w-24 bg-green-500 rounded-lg text-white">
+            <button onClick={handleSubmit} className="btn p-2 w-24 bg-green-500 rounded-lg text-white">
               Create
             </button>
           </>
         ) : (
           <>
-            <button onClick={handleDelete} className="p-2 w-24 bg-red-500 rounded-lg  text-white">
+            <button onClick={handleDelete} className="btn p-2 w-24 bg-red-500 rounded-lg  text-white">
               Delete
             </button>
-            <button onClick={handleSubmit} className="p-2 w-24 bg-blue-500 rounded-lg text-white">
+            <button onClick={handleSubmit} className="btn p-2 w-24 bg-blue-500 rounded-lg text-white">
               Edit
             </button>
           </>
