bab6d8a9c3fcc3435bb69a18d25f00c8fefdbd46
zlatonn <tonkar2541@hotmail.com>
2025-01-30 18:00:22 +0700

    refactor: move navbar to component
    - move navbar to ItemList
    - move navbar to ItemForm

--- frontend/src/pages/ItemList.tsx
+++ frontend/src/pages/ItemList.tsx
@@ -4,6 +4,7 @@ import { AxiosError } from "axios";
 
 import Item from "../components/item/Item";
 import useSearchQuery from "../stores/useSearchQuery";
+import NavBar from "../components/navbar/Navbar";
 
 // Define items type
 interface Items {
@@ -64,16 +65,19 @@ const ItemList = () => {
   // Return JSX with loading condition
   if (isLoading) {
     return (
-      <div className="container mx-auto py-10 px-10 sm:px-15 lg:px-20">
-        <div className=" mt-10 grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-6">
-          <div className="skeleton h-32 bg-gray-100"></div>
-          <div className="skeleton h-32 bg-gray-100"></div>
-          <div className="skeleton h-32 bg-gray-100"></div>
-          <div className="skeleton h-32 bg-gray-100"></div>
-          <div className="skeleton h-32 bg-gray-100"></div>
-          <div className="skeleton h-32 bg-gray-100"></div>
+      <>
+        <NavBar />
+        <div className="container mx-auto py-10 px-10 sm:px-15 lg:px-20">
+          <div className=" mt-10 grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-6">
+            <div className="skeleton h-32 bg-gray-100"></div>
+            <div className="skeleton h-32 bg-gray-100"></div>
+            <div className="skeleton h-32 bg-gray-100"></div>
+            <div className="skeleton h-32 bg-gray-100"></div>
+            <div className="skeleton h-32 bg-gray-100"></div>
+            <div className="skeleton h-32 bg-gray-100"></div>
+          </div>
         </div>
-      </div>
+      </>
     );
   }
 
@@ -81,56 +85,62 @@ const ItemList = () => {
   if (isError) {
     const axiosError = error as AxiosError; //Change error to AxiosError Type
     return (
-      <div className="mt-10 flex flex-col items-center gap-5">
-        {axiosError.request ? (
-          <p className="text-5xl text-gray-500">500</p>
-        ) : (
-          <p className="text-5xl text-gray-500">{axiosError.response?.status}</p>
-        )}
-        <p className="text-xl text-gray-400">{axiosError.message}</p>
-      </div>
+      <>
+        <NavBar />
+        <div className="mt-10 flex flex-col items-center gap-5">
+          {axiosError.request ? (
+            <p className="text-5xl text-gray-500">500</p>
+          ) : (
+            <p className="text-5xl text-gray-500">{axiosError.response?.status}</p>
+          )}
+          <p className="text-xl text-gray-400">{axiosError.message}</p>
+        </div>
+      </>
     );
   }
 
   //Return JSX with normal condition
   return (
-    <div className="container mx-auto py-10 px-10 sm:px-15 lg:px-20">
-      <div className="flex flex-col">
-        <div className="flex justify-end gap-3">
-          <p className="text-gray-800">Show :</p>
-          <select
-            value={selectDisplay}
-            onChange={(e) => setSelectDisplay(e.target.value)}
-            className="text-gray-400 text-center outline-none"
-          >
-            <option value="all">all</option>
-            <option value="warranty">warranty</option>
-            <option value="nearExpire">near expire</option>
-            <option value="expired">expired</option>
-          </select>
+    <>
+      <NavBar />
+      <div className="container mx-auto py-10 px-10 sm:px-15 lg:px-20">
+        <div className="flex flex-col">
+          <div className="flex justify-end gap-3">
+            <p className="text-gray-800">Show :</p>
+            <select
+              value={selectDisplay}
+              onChange={(e) => setSelectDisplay(e.target.value)}
+              className="text-gray-400 text-center outline-none"
+            >
+              <option value="all">all</option>
+              <option value="warranty">warranty</option>
+              <option value="nearExpire">near expire</option>
+              <option value="expired">expired</option>
+            </select>
+          </div>
+          {currItems && currItems.length ? (
+            <div className=" mt-10 grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-6">
+              {currItems.map((item) => {
+                return (
+                  <Item
+                    key={item.id}
+                    id={item.id}
+                    itemName={item.itemName}
+                    serialNumber={item.serialNumber}
+                    remainDays={item.remainDays}
+                    isWarranty={item.isWarranty}
+                  />
+                );
+              })}
+            </div>
+          ) : (
+            <div className="mt-10">
+              <p className="text-2xl text-gray-800">No items</p>
+            </div>
+          )}
         </div>
-        {currItems && currItems.length ? (
-          <div className=" mt-10 grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-6">
-            {currItems.map((item) => {
-              return (
-                <Item
-                  key={item.id}
-                  id={item.id}
-                  itemName={item.itemName}
-                  serialNumber={item.serialNumber}
-                  remainDays={item.remainDays}
-                  isWarranty={item.isWarranty}
-                />
-              );
-            })}
-          </div>
-        ) : (
-          <div className="mt-10">
-            <p className="text-2xl text-gray-800">No items</p>
-          </div>
-        )}
       </div>
-    </div>
+    </>
   );
 };
 export default ItemList;
