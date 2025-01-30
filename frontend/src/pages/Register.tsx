06cca67e7bd58ad5cc04522510db3d3ec08576b7
zlatonn <tonkar2541@hotmail.com>
2025-01-30 14:32:47 +0700

    feat: implement tepo register form
    - create form state
    - create error state
    - create function handleInputChange
    - create function validate form
    - create function handle submit
    - implement JSX

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,8 +1,61 @@
 import { Link } from "react-router-dom";
+import { ChangeEvent, useState } from "react";
 
 import bgUser from "../assets/bg_register.jpg";
 
+//Define type of form
+interface Iform {
+  email: string;
+  username: string;
+  password: string;
+}
+
+//Define type of form error
+interface IformErrors {
+  email?: string;
+  username?: string;
+  password?: string;
+}
+
 const Register = () => {
+  // Create form register
+  const [formData, setFormData] = useState<Iform>({
+    email: "",
+    username: "",
+    password: "",
+  });
+
+  // Create state errors
+  const [errors, setErrors] = useState<IformErrors>({});
+
+  // Function handle input change
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  // Function valid form
+  const validForm = (body: Iform) => {
+    const validErrors: IformErrors = {};
+    if (!body.email) validErrors.email = "*** Email is required ***";
+    if (!body.username) validErrors.username = "*** Username is required ***";
+    if (!body.password) validErrors.password = "*** Password end date is required ***";
+
+    return validErrors;
+  };
+
+  // Function handle submit
+  const handleSubmit = () => {
+    const invalid: IformErrors = validForm(formData);
+    setErrors(invalid);
+    if (Object.keys(invalid).length === 0) {
+      console.log(formData);
+    }
+  };
+
   return (
     <div className="w-full min-h-screen flex flex-col lg:flex-row">
       {/* Image Section */}
@@ -34,25 +87,51 @@ const Register = () => {
                 <p className="font-semibold text-gray-800">
                   Email<sup>*</sup>
                 </p>
-                <input type="text" placeholder="Email" className="input input-bordered w-full h-10" />
+                <input
+                  type="email"
+                  name="email"
+                  value={formData.email}
+                  onChange={handleInputChange}
+                  placeholder="Email"
+                  className="input input-bordered w-full h-10"
+                />
                 <p className="text-xs text-gray-500">Please enter your email.</p>
+                {errors.email && <p className="w-fit mt-1 text-red-500 text-xs">{errors.email}</p>}
               </div>
               <div className="flex flex-col gap-1 text-left">
                 <p className="font-semibold text-gray-800">
                   Username<sup>*</sup>
                 </p>
-                <input type="text" placeholder="Username" className="input input-bordered w-full h-10" />
+                <input
+                  type="text"
+                  name="username"
+                  value={formData.username}
+                  onChange={handleInputChange}
+                  placeholder="Username"
+                  className="input input-bordered w-full h-10"
+                />
                 <p className="text-xs text-gray-500">Choose a unique username.</p>
+                {errors.username && <p className="w-fit mt-1 text-red-500 text-xs">{errors.username}</p>}
               </div>
               <div className="flex flex-col gap-1 text-left">
                 <p className="font-semibold text-gray-800">
                   Password<sup>*</sup>
                 </p>
-                <input type="password" placeholder="Password" className="input input-bordered w-full h-10" />
+                <input
+                  type="password"
+                  name="password"
+                  value={formData.password}
+                  onChange={handleInputChange}
+                  placeholder="Password"
+                  className="input input-bordered w-full h-10"
+                />
                 <p className="text-xs text-gray-500">Create a strong password.</p>
+                {errors.password && <p className="w-fit mt-1 text-red-500 text-xs">{errors.password}</p>}
               </div>
             </div>
-            <button className="w-full my-10 bg-black text-white py-3 rounded-lg hover:shadow-lg">Sign up</button>
+            <button onClick={handleSubmit} className="w-full my-10 bg-black text-white py-3 rounded-lg hover:shadow-lg">
+              Sign up
+            </button>
           </div>
         </div>
       </div>
