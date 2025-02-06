fefaba03de1fd92a7f674da643f44b31bef38610
zlatonn <tonkar2541@hotmail.com>
2025-02-06 15:36:30 +0700

    refactor: implement useForm in Register component
    - register form using useForm to validate and submit form
    - change function on submit login
    - add correct symbol when valid input

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
-import { ChangeEvent, useState } from "react";
+import { useEffect, useState } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
 
 import { useRegister } from "../hooks/useApi";
 
@@ -17,23 +18,14 @@ interface Iform {
   password: string;
 }
 
-//Define type of form error
-interface IformErrors {
-  email?: string;
-  username?: string;
-  password?: string;
-}
-
 const Register = () => {
-  // Create state form register
-  const [formData, setFormData] = useState<Iform>({
-    email: "",
-    username: "",
-    password: "",
-  });
-
-  // Create state form errors
-  const [formErrors, setFormErrors] = useState<IformErrors>({});
+  // Register form to validation and submission
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<Iform>();
 
   //Create state http status error
   const [statusError, setStatusError] = useState<number | null>(null);
@@ -41,33 +33,36 @@ const Register = () => {
   // Fetch register user using userRegister
   const { mutate: registerUser } = useRegister(setStatusError);
 
-  // Function handle input change
-  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    setFormData((prev) => ({
-      ...prev,
-      [name]: value,
-    }));
+  // Function on submit
+  const onSubmit: SubmitHandler<Iform> = (data) => {
+    registerUser(data);
   };
 
-  // Function valid form
-  const validForm = (body: Iform) => {
-    const validErrors: IformErrors = {};
-    if (!body.email) validErrors.email = "*** Email is required ***";
-    if (!body.username) validErrors.username = "*** Username is required ***";
-    if (!body.password) validErrors.password = "*** Password end date is required ***";
+  // Watch form values
+  const email = watch("email");
+  const username = watch("username");
+  const password = watch("password");
 
-    return validErrors;
-  };
+  // Create state for track valid status
+  const [isEmailValid, setIsEmailValid] = useState(false);
+  const [isUsernameValid, setIsUsernameValid] = useState(false);
+  const [isPasswordValid, setIsPasswordValid] = useState(false);
 
-  // Function handle submit
-  const handleSubmit = () => {
-    const invalid: IformErrors = validForm(formData);
-    setFormErrors(invalid);
-    if (Object.keys(invalid).length === 0) {
-      registerUser(formData);
-    }
-  };
+  // Check isValid pattern
+  useEffect(() => {
+    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    setIsEmailValid(emailPattern.test(email));
+  }, [email]);
+
+  useEffect(() => {
+    const usernamePattern = /^[a-zA-Z0-9]{3,20}$/;
+    setIsUsernameValid(usernamePattern.test(username));
+  }, [username]);
+
+  useEffect(() => {
+    const passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,20}$/;
+    setIsPasswordValid(passwordPattern.test(password));
+  }, [password]);
 
   // Return JSX with error condition
   if (statusError !== null) {
@@ -106,61 +101,80 @@ const Register = () => {
               <span className="underline cursor-pointer hover:text-blue-500">Sign in</span>
             </Link>
           </div>
-          <div className="sm:px-10 md:px-28 lg:px-14 xl:px-28 2xl:px-40">
+          <form className="sm:px-10 md:px-28 lg:px-14 xl:px-28 2xl:px-40" onSubmit={handleSubmit(onSubmit)}>
             <h1 className="text-xl lg:text-2xl font-bold py-10">
               <span className="text-blue-500">Sign up</span> to Warranty Checker
             </h1>
             <div className="flex flex-col gap-3 sm:gap-5">
-              <div className="flex flex-col gap-1 text-left">
+              <div className="relative flex flex-col gap-1 text-left">
                 <p className="font-semibold text-gray-800">
                   Email<sup>*</sup>
                 </p>
                 <input
                   type="email"
-                  name="email"
-                  value={formData.email}
-                  onChange={handleInputChange}
                   placeholder="Email"
                   className="input input-bordered w-full h-10"
+                  {...register("email", {
+                    required: "Email is required.",
+                    pattern: {
+                      value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                      message: "Invalid email format.",
+                    },
+                  })}
                 />
                 <p className="text-xs text-gray-500">Please enter your email.</p>
-                {formErrors.email && <p className="w-fit mt-1 text-red-500 text-xs">{formErrors.email}</p>}
+                {isEmailValid && <span className="absolute right-3 top-9">✅</span>}
+                {errors.email && <p className="w-fit mt-1 text-red-500 text-xs py-1 px-2 bg-red-100 rounded-md">{errors.email.message}</p>}
               </div>
-              <div className="flex flex-col gap-1 text-left">
+              <div className="relative flex flex-col gap-1 text-left">
                 <p className="font-semibold text-gray-800">
                   Username<sup>*</sup>
                 </p>
                 <input
                   type="text"
-                  name="username"
-                  value={formData.username}
-                  onChange={handleInputChange}
                   placeholder="Username"
                   className="input input-bordered w-full h-10"
+                  {...register("username", {
+                    required: "Username is required.",
+                    pattern: {
+                      value: /^[a-zA-Z0-9]{3,20}$/,
+                      message: "Invalid username format.",
+                    },
+                  })}
                 />
                 <p className="text-xs text-gray-500">Choose a unique username.</p>
-                {formErrors.username && <p className="w-fit mt-1 text-red-500 text-xs">{formErrors.username}</p>}
+                {isUsernameValid && <span className="absolute right-3 top-9">✅</span>}
+                {errors.username && (
+                  <p className="w-fit mt-1 text-red-500 text-xs py-1 px-2 bg-red-100 rounded-md">{errors.username.message}</p>
+                )}
               </div>
-              <div className="flex flex-col gap-1 text-left">
+              <div className="relative flex flex-col gap-1 text-left">
                 <p className="font-semibold text-gray-800">
                   Password<sup>*</sup>
                 </p>
                 <input
                   type="password"
-                  name="password"
-                  value={formData.password}
-                  onChange={handleInputChange}
                   placeholder="Password"
                   className="input input-bordered w-full h-10"
+                  {...register("password", {
+                    required: "Password is required.",
+                    pattern: {
+                      value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,20}$/,
+                      message: "Password must contain at least one uppercase, one lowercase and one number.",
+                    },
+                  })}
                 />
-                <p className="text-xs text-gray-500">Create a strong password.</p>
-                {formErrors.password && <p className="w-fit mt-1 text-red-500 text-xs">{formErrors.password}</p>}
+                <p className="text-xs text-gray-500">Create a strong password at least 8 characters.</p>
+                {isPasswordValid && <span className="absolute right-3 top-9">✅</span>}
+                {errors.password && (
+                  <p className="w-fit mt-2 text-red-500 text-xs py-1 px-2 bg-red-100 rounded-md">{errors.password.message}</p>
+                )}
               </div>
             </div>
-            <button onClick={handleSubmit} className="w-full my-10 bg-black text-white py-3 rounded-lg hover:shadow-lg">
+            <button type="submit" className="w-full my-10 bg-black text-white py-3 rounded-lg hover:shadow-lg">
               Sign up
             </button>
-          </div>
+          </form>
         </div>
       </div>
     </div>
