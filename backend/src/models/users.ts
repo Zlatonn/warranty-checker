78ba4a422dc7cdade5d449d9096f329331faf966
zlatonn <tonkar2541@hotmail.com>
2025-02-17 02:39:43 +0700

    refactor: create model
    - user model

---
+++ backend/src/models/users.ts
@@ -0,0 +1,10 @@
+import { User } from "../types/authType";
+
+// Store users
+export const users: User[] = [
+  {
+    email: "warranty@mail.com",
+    username: "warranty001",
+    password: "$2b$10$XUp3ghr7nNqb./YbLjM9i.4ksxAu0ZhfU39WDVaQ4L8yfth1inmSa",
+  },
+];
